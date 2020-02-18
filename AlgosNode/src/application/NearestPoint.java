544da837ed884f6b7281d21c0563fe63d7dbdd83
77ramesh <47748256+77ramesh@users.noreply.github.com>
2020-02-18 20:14:19 +0545

    Add files via upload

--- AlgosNode/src/application/NearestPoint.java
+++ AlgosNode/src/application/NearestPoint.java
@@ -2,12 +2,14 @@ package application;
 
 import java.awt.Point;
 import java.util.LinkedList;
+import java.util.List;
 import java.util.Queue;
 
 public class NearestPoint {
 
-	private Point startPoint, nearestPoint;
+	private Point startPoint, nearestPoint, nearestConnection;
 	private Nodes nodes;
+	private List<Point> points;
 
 	public NearestPoint() {}
 	
@@ -17,9 +19,35 @@ public class NearestPoint {
 		
 	}
 	
+	public NearestPoint(List<Point> points, Point start) {
+		this.points = points;
+		this.startPoint = start;
+	}
+	
 	public Point getNearestPoint() {	
 		findNearestPoint();
 		return nearestPoint;
+	}
+	
+	public Point getNearestConnection() {
+		findNearestConnection();
+		return nearestConnection;
+	}
+	
+	private void findNearestConnection() {
+		
+		nearestConnection = points.get(0);
+		double closestDist = dist(startPoint.x, nearestConnection.x, startPoint.y, nearestConnection.y);
+		
+		for(int i=0;i<points.size();i++) {
+			 Point p = points.get(i);
+			 double dist = dist(startPoint.x, p.x, startPoint.y, p.y);
+			 
+			 if(dist < closestDist && dist != 0) {
+				 closestDist = dist;
+				 nearestConnection = points.get(i);
+			 }
+		}
 	}
 	
 	private void findNearestPoint() {
@@ -59,12 +87,12 @@ public class NearestPoint {
 	 * that will not be targeting to any
 	 * other point or node
 	 * */
-//	public Queue<Point> pointPath(Nodes.Node node) {
-//		
-//		Queue<Point> pq = new LinkedList<Point>();
-//		
-//		
-//	}
+	public Queue<Point> pointPath(Nodes.Node node) {
+		
+		Queue<Point> pq = new LinkedList<Point>();
+		
+		return pq;
+	}
 	
 	//d = (x2 - x1 )^2 + (y2 - y1)^2 under root
 	private double dist(double x1, double x2, double y1, double y2) {
